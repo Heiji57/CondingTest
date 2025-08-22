50ad3ade742c8116bdc7bb979b5b7b44b7edbaeb
Heiji57 <kjone0kjone0591@gmail.com>
2025-08-22 15:12:31 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 23020 KB -BaekjoonHub

---
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < K; i++) {
+            int money = Integer.parseInt(br.readLine());
+            if (money == 0) {
+                stack.pop();
+            } else {
+                stack.push(money);
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < stack.size(); i++) {
+            result += stack.get(i);
+        }
+
+        System.out.println(result);
+    }
+}
