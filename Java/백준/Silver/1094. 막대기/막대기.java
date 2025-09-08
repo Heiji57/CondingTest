18dc8cc1867ecbb4e09f8e2509c71fbd52855cf3
Heiji57 <kjone0kjone0591@gmail.com>
2025-09-08 23:47:52 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ Java/백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+
+        int result = Integer.bitCount(X);
+        System.out.println(result);
+    }
+}
