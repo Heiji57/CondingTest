4cda64b13ae10ea4e0a4898960236866ac6450aa
Heiji57 <kjone0kjone0591@gmail.com>
2026-01-19 20:01:29 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 24.66 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        return (int) Arrays.stream(Integer.toString(n, k).split("0"))
+                .filter(s -> !s.isEmpty() && !s.equals("1"))
+                .mapToLong(Long::parseLong)
+                .filter(num -> {
+                    for (long i = 2; i <= Math.sqrt(num); i++)
+                        if (num % i == 0) return false;
+                    return true;
+                })
+                .count();
+    }
+}
